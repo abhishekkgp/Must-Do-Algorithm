595d46bfea16d3aea39f7dcfcf593d63e406f10a
Abhishek Kumar <abhisheksingnbr@gmail.com>
2021-11-26 16:16:04 +0530

    Create KMP-Algorithm.cpp

---
+++ KMP-Algorithm.cpp
@@ -0,0 +1,77 @@
+Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) 
+that prints all occurrences of pat[] in txt[]. You may assume that n > m.
+
+  
+  time complexity: O(n) where n is length of txt.
+  space complexity: O(m) where m is length of pattern 'pat'
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+
+void computeLPSArray(char* pat, int M, int* lps);
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char* pat, char* txt)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+	int lps[M];
+	computeLPSArray(pat, M, lps);
+
+	int i = 0; // index for txt[]
+	int j = 0; // index for pat[]
+	while (i < N) {
+		if (pat[j] == txt[i]) {
+			j++;
+			i++;
+		}
+
+		if (j == M) {
+			printf("Found pattern at index %d ", i - j);
+			j = lps[j - 1];
+		}
+		else if (i < N && pat[j] != txt[i]) {
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
+}
+
+// Fills lps[] for given patttern pat[0..M-1]
+void computeLPSArray(char* pat, int M, int* lps)
+{
+	int len = 0;// length of the previous longest prefix suffix
+	lps[0] = 0; // lps[0] is always 0
+  
+	int i = 1;// the loop calculates lps[i] for i = 1 to M-1
+	while (i < M) {
+		if (pat[i] == pat[len]) {
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else 
+		{
+			if (len != 0) {
+				len = lps[len - 1];
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+// Driver program to test above function
+int main()
+{
+	char txt[] = "ABABDABACDABABCABAB";
+	char pat[] = "ABABCABAB";
+	KMPSearch(pat, txt);
+	return 0;
+}
